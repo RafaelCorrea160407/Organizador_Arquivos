247c25de2f24d3ac14c895484e94ba8c6091438a
Rafael <202502905408@alunos.ibmec.edu.br>
2026-07-13 17:23:53 -0300

    primeira versão do organizador de arquivos usando pathlib

--- org.py
+++ org.py
@@ -11,10 +11,22 @@ pasta = Path("C:/Users/Usuário/Documents/Organizador_Arquivos/testes")
 
 
 
+
 for arquivo in pasta.glob("*"):
     if arquivo.is_file():
        sufixo = arquivo.suffix
        destino = arquivos.get(sufixo)
+    if destino == None:
+        pasta_destino = pasta / "Outros"
+    else:
+        pasta_destino = pasta / destino
+        
+    arquivo_destino = pasta_destino / arquivo.name
+    pasta_destino.mkdir(exist_ok=True, parents=True)
+    arquivo.rename(arquivo_destino)
+      
+       
+       
        
        
         
